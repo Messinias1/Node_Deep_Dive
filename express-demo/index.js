ae58810e6d7d44fd309d081f323b8c92cbf97506
Carl Kakisis <Messinias1@gmail.com>
2020-01-09 00:20:50 -0500

    config

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const config = require("config");
 const app = express();
 const Joi = require("joi");
 const helmet = require("helmet");
@@ -11,6 +12,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(helmet());
 app.use(morgan("tiny"));
+
+// Configuration
+console.log("Application Name: " + config.get("name"));
+console.log("Mail Server: " + config.get("mail.host"));
+console.log("Mail Password: " + config.get("mail.password"));
+
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+  console.log("Morgan enabled...");
+}
 
 app.use(logger);
 app.use(authenticate);
