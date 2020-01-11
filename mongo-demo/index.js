cbb4997b937435e28a67f4616c2ac17565e9e515
Carl Kakisis <Messinias1@gmail.com>
2020-01-11 18:46:07 -0500

    updateing documents

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost/playground")
+  .connect("mongodb://localhost/playground", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log("Connected to MongoDB..."))
   .catch(err => console.error("could not connect to mongoDB...", err));
 
@@ -45,12 +48,46 @@ async function getCourses() {
   // /^Mosh/ = starts with
   // /Hamedani$/ = ends with
   // /.*Mosh.*/ = contains
+
+  const pageNumber = 2;
+  const pageSize = 10;
+
   const courses = await Course.find({ author: "Mosh", isPublished: true })
-    .limit(10)
+    .skip((pageNumber - 1) * pageSize)
+    .limit(pageSize)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 });
-  // .count()
   console.log(courses);
 }
+// getCourses();
 
-getCourses();
+async function updateCourse(id) {
+  // Approach: Query First
+  // findById()
+  // Modify its properties
+  // save()
+  //   const course = await Course.findById(id);
+  //   if (!course) return;
+
+  //   course.isPublished = true;
+  //   course.author = "Another Author";
+
+  //   const result = await course.save();
+  //   console.log(result);
+
+  // Approach: Update First
+  // Update Directly
+  // Optionally: get the updated document
+  const course = await Course.findByIdAndUpdate(
+    id,
+    {
+      $set: {
+        author: "Jason",
+        isPublished: false
+      }
+    },
+    { new: true }
+  );
+  console.log(course);
+}
+updateCourse("5e1a28938abb6fb3705e09e4");
