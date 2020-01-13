1b0267a98bc4f94677cd6dcef861a510e63bca18
Carl Kakisis <Messinias1@gmail.com>
2020-01-13 14:01:29 -0500

    restuctured customers file

--- 2020-09-01restructure/vidly/index.js
+++ 2020-09-01restructure/vidly/index.js
@@ -1,23 +1,20 @@
-const Joi = require("joi");
+const mongoose = require("mongoose");
+const genres = require("./routes/genres");
+const customers = require("./routes/customers");
 const express = require("express");
-const logger = require("./logger");
-const authenticator = require("./authenticator");
 const app = express();
 
+mongoose
+  .connect("mongodb://localhost/vidly", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch(err => console.error("could not connect to mongoDB...", err));
+
 app.use(express.json());
-
-app.use(logger);
-app.use(authenticator);
-
-function validateGenre(genre) {
-  const schema = {
-    name: Joi.string()
-      .min(3)
-      .required()
-  };
-
-  return Joi.validate(genre, schema);
-}
+app.use("/api/genres", genres);
+app.use("/api/customers", customers);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
